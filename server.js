5573aece5412f14c7b40dfd858e9bc775a1ad48d
daniyaldev2k20 <68988102+daniyaldev2k20@users.noreply.github.com>
2020-11-27 11:53:23 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,56 +2,58 @@
 // where your node app starts
 
 // init project
-var express = require("express");
+var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC
-var cors = require("cors");
-app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 204
+// so that your API is remotely testable by FCC 
+var cors = require('cors');
+app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
-app.use(express.static("public"));
+app.use(express.static('public'));
 
 // http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
+  res.sendFile(__dirname + '/views/index.html');
 });
 
-// your first API endpoint...
+
+// your first API endpoint... 
 app.get("/api/hello", function (req, res) {
-  res.json({ greeting: "hello API" });
+  res.json({greeting: 'hello API'});
 });
 
-// API endpoint as requested in user stories
-app.get("/api/timestamp/:dateString?", (req, res) => {
-  const dateString = req.params.dateString;
-  let date;
+app.get('/api/timestamp/:date?', (req, res) => {
+  let date_string = req.params.date;
 
-  if (!dateString) {
-    date = new Date();
-  } else {
-    if (Number.isNaN(dateString)) {
-      date = new Date(dateString);
-    } else {
-      date = new Date(parseInt(dateString));
-    }
+  if(!date_string){
+    return res.json({
+      unix: Date.now(),
+      utc: Date()
+    })
   }
 
-  // if the date is invalid send the error response
-  // else send the response in the requested format
-  if (date.toString() === "Invalid Date") {
+  if(Number.isNaN(date_string)){
+    date_string = new Date(date_string);
+  }
+  else{
+    date_string = new Date(parseInt(date_string))
+  }
+
+  if (date_string.toString() === 'Invalid Date') {
     res.send({
-      error: date.toString(),
+      error: date_string.toString(),
     });
   } else {
     res.send({
-      unix: date.getTime(),
-      utc: date.toUTCString(),
+      unix: date_string,
+      utc: date_string.toUTCString(),
     });
   }
-});
+})
+
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
-  console.log("Your app is listening on port " + listener.address().port);
+  console.log('Your app is listening on port ' + listener.address().port);
 });
